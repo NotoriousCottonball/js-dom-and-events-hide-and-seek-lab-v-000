c4ade85b0decc864ba28c977884202c7581d10fa
George  Pianka <gpianka1@gmail.com>
2019-04-27 06:15:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+ function nestedTarget(nestedTarget){
+ return document.querySelector('#nested .target');
+}
+
+ function deepestChild(){  //deepest node in #grand-node ('boo' 4 divs deep)
+  return document.querySelector('#grand-node div div div div');
+}
+
+ function increaseRankBy(n){
+  const rankedList = document.querySelectorAll('.ranked-list');
+  for (let i = 0; i < rankedList.length; i++) {
+    rankedList[i].innerHTML = (i + 1).toString();
+  }
+} 
