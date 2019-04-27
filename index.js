e2f9746d28fdd185cf38daaa9e712ce838ac922f
George  Pianka <gpianka1@gmail.com>
2019-04-27 06:30:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function getFirstSelector(selector){
      for (let i = 0, l = rankedLists.length; i < l; i++) {
         let children = rankedLists[i].children
 
-         for (let i = 0, k = children.length; i < k; j++) {
-            children[i].innerHTML = parseInt(children[i].innerHTML) + n;
+         for (let j = 0, k = children.length; j < k; j++) {
+            children[j].innerHTML = parseInt(children[j].innerHTML) + n;
         }
     }
 }
